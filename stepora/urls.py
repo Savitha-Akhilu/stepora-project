96e6bfbbbe20f6376c933ac2544808861c031221
Savitha <savithan156@gmail.com>
2025-10-16 13:52:40 +0530

    Users urls added to settings.py

--- stepora/urls.py
+++ stepora/urls.py
@@ -20,4 +20,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('adminpanel/', include('adminpanel.urls')),
+    path('users/', include('user_section.urls')),
+
 ]
